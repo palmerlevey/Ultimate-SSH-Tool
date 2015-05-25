6e1ff555a838acb5ebbf41f8ead7acd91051ef37
David Levey <dlevey@knownhost.com>
2015-05-25 07:09:53 -0500

    PSA version check

--- checks/PSA.sh
+++ checks/PSA.sh
@@ -1,1 +1,7 @@
-echo "Plesk Version: $(egrep -o '[0-9]?[0-9]\.[0-9]?[0-9]\.[0-9]?[0-9]' /usr/local/psa/core.version)";
+awk -v SGPASS=$SGPASS -v SGFAIL=$SGFAIL -v SGWARN=$SGWARN '{
+	if($1 ~ /^12.[0-9]?[0-9].[0-9]?[0-9]/){ 
+		print "Plesk Version: " $0 SGPASS  
+	} else {
+		print "Plesk Version: " $0 SGFAIL 
+	}
+}' /usr/local/psa/core.version
