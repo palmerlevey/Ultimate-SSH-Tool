519bf0df05a110b192d32b1a1c05832936a01276
David Levey <dlevey@knownhost.com>
2015-08-12 11:36:43 -0500

    yay tweaks!

--- server_crons/cmsvlcmds.d/blogs/bolt.sh
+++ server_crons/cmsvlcmds.d/blogs/bolt.sh
@@ -4,15 +4,12 @@ cmsname='Bolt';
 
 # CMS Info/Description
 cmshomepage='https://bolt.cm';
-#cmslatestvurl='https://bolt.cm/'; # Currently experiencing SSL issues.
-cmslatestvurl='http://bolt.cm/';
+cmslatestvurl='https://bolt.cm/';
 cmsdescrip='NA';
 cmsfindinstalledvi='NA';
 
 # Additional curl arguments required.
-curlargs='\
--A "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5" \
--s ';
+curlargs='-ks ';
 
 # Run version lookup command.
 cmslatestversion=$(curl $curlargs $cmslatestvurl |grep -Po "version v (\d{1,2})\.(\d{1,2})?\.(\d{1,2})" |sed 's/version v //g');
