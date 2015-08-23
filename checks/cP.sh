92f78bb54b7c2dc0d45a9622b926c5c53ff5535b
David P. Levey, Jr <david@palmerlevey.com>
2015-08-23 07:35:17 -0500

    added DBarbers changes

--- checks/cP.sh
+++ checks/cP.sh
@@ -1,31 +1,48 @@
-/usr/local/cpanel/bin/rebuild_phpconf --current | awk -v SGPASS=$SGPASS -v SGFAIL=$SGFAIL -v SGWARN=$SGWARN -v SGSEC=$SGSEC '/PHP5/ {
-	if( $3 ~ /[Ss][Uu][Pp][Hh][Pp]/ ) {
-		print $0 SGPASS
-		while ((getline line < "/etc/csf/csf.pignore") > 0) {
-			if (line ~ /^[[:blank:]]*p?(cmd|exe):\/usr\/bin\/php[[:blank:]]*$/) {
-				print SGSEC " Remove " line " from csf.pignore" SGFAIL
-			}
-		}
-	} else if( $3 ~ /[Ff][Cc][Gg][Ii]/ ) {
-		print $0 SGWARN
+SGINFO="\\033[60G\\033[0;39m[\\033[1;34mINFO\\033[0;39m]"
+
+/usr/local/cpanel/bin/rebuild_phpconf --current | awk -v SGPASS=$SGPASS -v SGFAIL=$SGFAIL -v SGWARN=$SGWARN -v SGSEC=$SGSEC '/PHP5/ { HAND=$0 } /RUID/ { DSOR=$0
+	if( HAND ~ /[Ss][Uu][Pp][Hh][Pp]/ ) {
+	        print HAND SGPASS
+    	} else if( HAND ~ /[Dd][Ss][Oo]/ && DSOR ~ /[Ee][Nn][Aa][Bb][Ll][Ee][Dd]/ ) {
+        	if( system( "test -f /usr/local/lsws/bin/lswsctrl" ) != 0 ) {
+            		print HAND " + mod_ruid" SGPASS
+        	} else {
+            		print HAND " + mod_ruid is incompatible with LiteSpeed" SGFAIL
+        	}            
+	} else if( HAND ~ /[Ff][Cc][Gg][Ii]/ ) {
+		print HAND SGWARN
 	} else {
-		print $0 SGFAIL
-		while ((getline line < "/etc/csf/csf.pignore") > 0) {
-			if (line ~ /^[[:blank:]]*p?(cmd|exe):\/usr\/bin\/php[[:blank:]]*$/) {
-				print SGSEC " Remove " line " from csf.pignore" SGFAIL
-			}
-		}
+		print HAND SGFAIL
+	}
+
+	while ((getline line < "/etc/csf/csf.pignore") > 0) {
+		if (line ~ /^[[:blank:]]*p?(cmd|exe):\/usr\/bin\/php[[:blank:]]*$/) {
+            		print SGSEC " Remove " line " from csf.pignore" SGFAIL
+        	}
 	}
 }'
-
 awk -v SGPASS=$SGPASS -v SGFAIL=$SGFAIL -v SGWARN=$SGWARN '{
-	if($1 ~ /^11.[4-9][0-9]/){ 
-		print "cPanel Version: " $0 SGPASS  
-	} else {
-		print "cPanel Version: " $0 SGFAIL 
-	}
+        if($1 ~ /^11.[4-9][0-9]/){
+                print "cPanel Version: " $0 SGPASS
+        } else {
+                print "cPanel Version: " $0 SGFAIL
+        }
 }' /usr/local/cpanel/version
 
-if [ -e /usr/local/frontpage/version5.0/bin/owsadm.exe ]; then 
-	echo -e "Frontpage Installed" $SGFAIL;
+if [[ -f /usr/local/lsws/bin/lswsctrl ]]; then
+    echo -e "LiteSpeed is installed $SGINFO"
+fi
+
+if [[ -f /usr/sbin/nginx ]]; then
+    if [[ -f /usr/local/cpanel/whostmgr/docroot/cgi/nginx.php ]]; then
+        echo -e "nginxCP is installed $SGINFO"
+    elif [[ -f /usr/local/cpanel/whostmgr/docroot/cgi/engintron.php ]]; then
+        echo -e "nginx / Enginetron is installed $SGINFO"
+    else
+        echo -e "nginx binaries present; plugin unknown $SGWARN"
+    fi
+fi
+
+if [ -e /usr/local/frontpage/version5.0/bin/owsadm.exe ]; then
+        echo -e "Frontpage Installed" $SGFAIL;
 fi
